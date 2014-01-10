9282368f0b78dacfc61d9d4c46aa4a00f08674a0
Micah Nordland <mpnordland@gmail.com>
2014-01-10 13:51:57 -0500

    moved template into main.go and added support for images

--- main.go
+++ main.go
@@ -14,6 +14,19 @@ import (
 const DateFormat = "Jan _2 2006 15:04"
 
 
+const Tmpl = `---
+title: {{title}}
+author: {{author}}
+image: {{image}}
+tags: 
+{{#tags}}
+ - {{.}} {{! This wrong seeming setup means there won't be a extra newline between the tag list and the date }}
+{{/tags}}date: {{date}}
+---
+
+{{body}}`
+
+
 func input(field string, scanner *bufio.Scanner) string {
   fmt.Printf("%s:", field)
   scanner.Scan()
@@ -24,13 +37,14 @@ func collectData(scanner *bufio.Scanner) map[string]interface{} {
   metadata := make(map[string]interface{})
   metadata["title"] = input("Title", scanner)
   metadata["author"] = input("Author", scanner)
+  metadata["image"] = input("Image path", scanner)
   metadata["tags"]= strings.Split(input("Tags", scanner),", ")
   return metadata
 }
 
 func getBody(scanner *bufio.Scanner) string {
   tmpFile := "/tmp/coconut-post"
-  editor := "/usr/bin/vim" //strings.Split(os.Getenv("EDITOR"), " ")[0]
+  editor := os.Getenv("EDITOR")
   if editor == "" {
     fmt.Println("What editor would you like to write your post in?")
     scanner.Scan()
@@ -52,6 +66,10 @@ func getBody(scanner *bufio.Scanner) string {
 }
 
 func main() {
+  var afName string
+  if len(os.Args) > 1 {
+      afName = os.Args[1]
+  }
   scanner := bufio.NewScanner(os.Stdin)
   article := collectData(scanner)
   article["body"] = getBody(scanner)
@@ -59,13 +77,15 @@ func main() {
     fmt.Println("Aborting due to empty body")
     return
   }
-  afName := strings.ToLower(article["title"].(string))
-  if len(afName)> 20{
-    afName=afName[:20]
+  if afName == ""{
+    afName = strings.Replace(strings.ToLower(article["title"].(string)), " ", "-", -1)
+    if len(afName)> 20{
+        afName=afName[:20]
+    }
   }
   ctime := time.Now()
   article["date"] = ctime.Format(DateFormat)
-  rendered := mustache.RenderFile("post.tmpl", article)
+  rendered := mustache.Render(Tmpl, article)
   aFile, err := os.Create(afName+".md")
   defer aFile.Close()
   if err != nil{
